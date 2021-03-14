afaa5dd4e5c4aaa1690dd0c2985ae9e39e090be8
lkristof <ladanyi.kristof@gmail.com>
2021-03-14 15:01:27 +0100

    Add endpoint for image uploading

---
+++ src/InstagramClone/InstagramClone.Web/Controllers/ImageController.cs
@@ -0,0 +1,38 @@
+﻿using InstagramClone.Web.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace InstagramClone.Web.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ImageController : ControllerBase
+    {
+        private static List<Image> _images = new List<Image>();
+
+        [HttpPost("upload")]
+        public async Task<IActionResult> Upload([FromForm] string title, [FromForm] string uploader, [FromForm] string description, [FromForm] IFormFile file)
+        {
+            var newImage = new Image()
+            {
+                Title = title,
+                Description = description,
+                Uploader = uploader,
+                UploadDate = DateTime.Now,
+            };
+
+            using (var stream = new MemoryStream())
+            {
+                await file.CopyToAsync(stream);
+                newImage.ImageData = stream.ToArray();
+            }
+
+            _images.Add(newImage);
+            return Ok();
+        }
+    }
+}
