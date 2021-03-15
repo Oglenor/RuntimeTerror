04fd8ec7af1f1cbee877c9c033101b8d8fb589d5
Bence Palatin <palatinb@gmail.com>
2021-03-15 16:56:18 +0100

    Connect upload API with frontend

--- src/InstagramClone/InstagramClone.Web/ClientApp/src/components/Upload/Upload.js
+++ src/InstagramClone/InstagramClone.Web/ClientApp/src/components/Upload/Upload.js
@@ -1,24 +1,91 @@
-import React, {Component} from 'react';
-import {Container, Button, Input} from "reactstrap";
-import './Upload.css'
+import React, { Component } from "react";
+import {Input, Container, Button, Toast, ToastBody, Spinner} from "reactstrap";
+import "../Layout/Layout.css"
+import "./Upload.css";
+import axios from "axios";
 
-export class Upload extends Component {
-  static displayName = Upload.name;
+export class Upload extends  Component {
+    static displayName = Upload.Name;
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            success: false,
+            show: false,
+            showSpinner: false,
+            selectedFile: null,
+            selectedFilePreview: null,
+            description: ""
+        }
+        this.handlefileSelected = this.handlefileSelected.bind(this)
+        this.handleDescriptionChange = this.handleDescriptionChange.bind(this)
+    }
+
+    async upload() {
+        let imageDtoForm = new FormData();
+
+        imageDtoForm.append('Title', "sometitle");
+        imageDtoForm.append('Description', this.state.description);
+        imageDtoForm.append('File', this.state.selectedFile);
+
+        this.setState({showSpinner: true});
+        try {
+            const res = await axios.post('https://localhost:5001/image/upload', imageDtoForm);
+            this.setState({show: true, success: true});
+            console.log(res);
+        } catch (ex) {
+            console.log(ex);
+            this.setState({show: true});
+        }
+    }
+
+    handlefileSelected(event) {
+        console.log(event.target.files[0]);
+        this.setState({
+            selectedFile: event.target.files[0],
+            selectedFilePreview: URL.createObjectURL(event.target.files[0])
+        })
+    }
+
+    handleDescriptionChange(event) {
+        this.setState({description: event.target.value});
+        console.log(this.state.description);
+    }
 
 
-  render() {
-    return (
-        <Container>
-            <div className="upload-wrapper">
-                <h1 className="text-center page-header">Képfeltöltés</h1>
-
-                <div className="upload">
-                    <Input type="file"/>
-                    <h3 className="upload__title">Válassz képet</h3>
+    render() {
+        const state = this.state;
+        return (
+            <Container>
+                <Toast isOpen={state.show} className={this.state.success ? "toast success" : "toast fail"}>
+                    <ToastBody>
+                        {state.success ? "Uploaded!" : "Upload failed!"}
+                    </ToastBody>
+                </Toast>
+                <div className="upload-wrapper">
+                    <h1 className="text center page-header">Új post</h1>
+                    <div className="upload">
+                        <Input type="file" onChange={this.handlefileSelected}/>
+                        <h3 className="upload--title">Válassz képet!</h3>
+                    </div>
                 </div>
-                <Button color="primary" className="button-upload">Feltöltés</Button>
-            </div>
-        </Container>
-    );
-  }
+
+                {this.state.selectedFilePreview && (
+                    <div className="preview-wrapper">
+                        <img className="preview--img" src={this.state.selectedFilePreview} alt=""/>
+                        <p>Írj leírást a képhez:</p>
+                        <input
+                            type="text" value={this.state.description} onChange={this.handleDescriptionChange}
+                        />
+                        <Button color="primary" className="button-upload" onClick={() => this.upload()}>Feltöltés</Button>
+                    </div>
+                )}
+                {this.state.showSpinner && (
+                    <div className="upload--spinner">
+                        <Spinner color="primary" className="spinner"/>
+                    </div>
+                )}
+            </Container>
+        );
+    }
 }
